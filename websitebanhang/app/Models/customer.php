e3457b825fa063573a78a6b19801c4eed1ae82d1
HoangDinhHanh95 <kocamxuc114@gmail.com>
2021-07-04 01:33:23 +0700

    first commit

---
+++ websitebanhang/app/Models/customer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class customer extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+    protected $fillable = [
+        'customer_name','customer_email','customer_password','customer_address','customer_phone'
+    ];
+    
+    protected $primary = 'customer_id';
+    protected $table = 'tbl_customer';
+}
